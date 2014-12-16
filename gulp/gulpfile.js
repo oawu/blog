4a2e343fea0e6773ca091904fdbd6e113a6c0609
OA Wu <comdan66@gmail.com>
2014-12-16 22:17:10 +0800

    Create files.

---
+++ gulp/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require ('gulp'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload');
+
+gulp.task ('default', function () {
+  livereload.listen ();
+
+  ['./root/*.html', './root/css/**/*.css', './root/res/**/*.js', './root/js/**/*.js'].forEach (function (t) {
+    gulp.watch (t).on ('change', function () {
+      gulp.run ('reload');
+    });
+  });
+});
+
+gulp.task ('reload', function () {
+  livereload.changed ();
+  console.info ('\nReLoad Browser!\n');
+});
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Welcome To OA's blog!
+這是一個 OA's Blog。 
+
+---
+## 聲明
+本作品只限分享於研究、研討性質之使用，並不提供任何有營利效益之使用。  
+如有營利用途，務必告知作者 [OA](http://www.ioa.tw/) (<comdan66@gmail.com>)，並且經由作者同意。
+
+
+<br/>
+## 簡介
+* 這是一個 OA's Blog。 
+
+
+<br/>
+## 關於
+* 作者名稱 - [OA Wu](http://www.ioa.tw/)
+
+* E-mail - <comdan66@gmail.com>
+
+* 作品名稱 - OA-blog
+
+* GitHub - [OA-test](https://github.com/comdan66/OA-blog)
+
+* 更新日期 - 2014/12/15
