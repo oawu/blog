1b49903a79b197173f59f0bc99567defe70840db
OA Wu <comdan66@gmail.com>
2014-12-19 17:47:43 +0800

    Add message.

--- js/frame.js
+++ js/frame.js
@@ -51,7 +51,7 @@ $(function () {
 
   $('.oa-tree li:has(ul) > .folder').click (function () {
     var $ul = $(this).parent ('li').children ('ul'), $span = $(this).children ('span:first-child');
-    
+
     if ($ul.length && ($span.text (!$ul.hasClass ("tree-show") ? $span.data ('open') : $span.data ('close'))))
       if ($ul.hasClass ("tree-show")) $ul.removeClass ('tree-show');
       else $ul.addClass ('tree-show');
@@ -59,4 +59,33 @@ $(function () {
 
   $('.oa-tree li:first-child li:first-child li:first-child').parents ('ul').prev ('span').click ().parents ('ul').prev ('span');
 
+  if (typeof (Storage) !== 'undefined') {
+    var message = '', timeRange = 60 * 3, last = localStorage.getItem ('messages');
+    if (last) {
+      last = JSON.parse (last);
+      message = (Math.abs ($.timeago.parse (new Date ().getFullYear () + '-' + (new Date ().getMonth () + 1) + '-' + new Date ().getDate () + ' ' + new Date ().getHours () + ':' + new Date ().getMinutes () + ':' + new Date ().getSeconds ())) - Math.abs ($.timeago.parse (last.d.Y + '-' + last.d.M + '-' + last.d.D + ' ' + last.d.h + ':' + last.d.m + ':' + last.d.s))) / 1000 < timeRange ? '' : ('你在' + jQuery.timeago (last.d.Y + '-' + last.d.M + '-' + last.d.D + ' ' + last.d.h + ':' + last.d.m + ':' + last.d.s, true) + '有來過喔！' + (last.n && last.n.length ? '<br/>你上次看的是：<b>' + last.n + '</b>': ''));
+    } else {
+      message = 'Hi, 這是你第一次光臨耶！';
+    }
+    if (message.length) {
+      setTimeout (function () {
+        var $message = $('.pin.oa_infos .message').html (message).removeClass ('hide');
+        var obj = { v: last ? last.v + 1 : 1,
+                    d: {Y: new Date ().getFullYear (), M: new Date ().getMonth () + 1, D: new Date ().getDate (), h: new Date ().getHours (), m: new Date ().getMinutes (), s: new Date ().getSeconds ()},
+                    n: $message.data ('name') && $message.data ('name').length ? $message.data ('name') : (last ? last.n : ''),
+                    h: last ? [window.location.pathname].concat (last.h) : [window.location.pathname]
+                  };
+        localStorage.setItem ('messages', JSON.stringify (obj));
+        if (!$(window).scrollTop ()) {
+          $(window).scroll (function () {
+            $message.addClass ('hide');
+          });
+        } else {
+          setTimeout (function () {
+            $message.addClass ('hide');
+          }, 10000);
+        }
+      }, 1000);
+    }
+  }
 });
