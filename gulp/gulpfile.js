815ece2f8b7b7d1e5523ad0a9cd428299942b7fc
OA Wu <comdan66@gmail.com>
2014-12-22 15:25:50 +0800

    Edit gulpfile.

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require ('gulp'),
     notify = require('gulp-notify'),
-    livereload = require('gulp-livereload');
+    uglifyJS = require ('gulp-uglify'),
+    livereload = require('gulp-livereload'),
+    minifyHTML = require('gulp-minify-html'),
+    del = require('del');
 
 gulp.task ('default', function () {
   livereload.listen ();
@@ -15,4 +18,38 @@ gulp.task ('default', function () {
 gulp.task ('reload', function () {
   livereload.changed ();
   console.info ('\nReLoad Browser!\n');
+});
+
+gulp.task ('minify', function () {
+  gulp.run ('js-uglify');
+  gulp.run ('res-uglify');
+  gulp.run ('minify-html');
+});
+gulp.task ('gh-pages', function () {
+  del (['./root']);
+});
+gulp.task ('js-uglify', function () {
+  gulp.src ('./root/js/*.js')
+      .pipe (uglifyJS ())
+      .pipe (gulp.dest ('./root/js/'));
+});
+gulp.task ('res-uglify', function () {
+  gulp.src ('./root/res/**/*.js')
+      .pipe (uglifyJS ())
+      .pipe (gulp.dest ('./root/res/'));
+});
+gulp.task ('minify-html', function () {
+  var opts = {comments: true, spare:true};
+
+  gulp.src ('./root/article/**/*.html')
+      .pipe (minifyHTML (opts))
+      .pipe (gulp.dest ('./root/article/'))
+
+  gulp.src ('./root/articles/**/*.html')
+      .pipe (minifyHTML (opts))
+      .pipe (gulp.dest ('./root/articles/'))
+
+  gulp.src ('./root/tag/**/*.html')
+      .pipe (minifyHTML (opts))
+      .pipe (gulp.dest ('./root/tag/'))
 });
