3e27baf25eb693d76eb4e7484c8a7043eeb7d694
OA Wu <comdan66@gmail.com>
2021-07-01 17:36:51 +0800

    Fix

--- src/js/index.js
+++ src/js/index.js
@@ -70,7 +70,7 @@ Vue.component('main-article', {
     article#article
       header
         h1 => *text=article.title
-        span => *text=article.st   *if=article.st
+        span => *text=article.subtitle   *if=article.subtitle
 
       section
         el => *for=(el, i) in article.a   :key=i   :el=el
@@ -79,13 +79,13 @@ Vue.component('main-article', {
           h2 => *text='相關參考'
           el => *for=(el, i) in article.r   :key=i   :el=el
 
-      div#prev-next => *if=article.p || article.n   :class='n' + [article.p, article.n].filter(t => t).length
-        a.icon-a.prev => *if=article.p   :href=article.p.url
+      div#prev-next => *if=article.other   :class='n' + Object.keys(article.other).length
+        a.icon-a.prev => *if=article.other.prev   :href=article.other.prev.url
           span => *text='上一篇'
-          b => *text=article.p.t
-        a.icon-b.next => *if=article.n   :href=article.n.url
+          b => *text=article.other.prev.t
+        a.icon-b.next => *if=article.other.next   :href=article.other.next.url
           span => *text='下一篇'
-          b => *text=article.n.t
+          b => *text=article.other.next.t
       
       footer
         time => *text=Datetime(article.at).ago   :datetime=Datetime(article.at)
@@ -105,10 +105,8 @@ Vue.component('main-articles', {
 
       div.articles
         a => *for=(article, i) in articles   :key=i   :href=article.url
-          figure => *if=article.m
-            //img => :src=article.m
-          b => *text=article.t
-          span => *if=article.st   *text=article.st`)
+          b => *text=article.title
+          span => *if=article.subtitle   *text=article.subtitle`)
 })
 
 Vue.component('loading', {
@@ -154,33 +152,33 @@ Load.init({
     title: "OA Wu's Blog",
     item: null,
     items: [
-      { k: 'index',   st: 0, c: 'd', t: '個人簡介' },
-      { k: 'dev',     st: 0, c: 'e', t: '開發心得' },
-      { k: 'mac',     st: 0, c: '1b', t: '蘋果環境' },
-      { k: 'aws',     st: 0, c: '22', t: 'AWS筆記' },
-      { k: 'note',    st: 0, c: '1e', t: '隨筆雜記' },
-      { k: 'mazu',    st: 0, c: '1a', t: '鄉土北港' },
-      { k: 'unbox',   st: 0, c: 'f', t: '開箱文章' },
-      { k: 'album',   st: 0, c: '14', t: '生活相簿' },
-      { k: 'license', st: 0, c: '16', t: '授權聲明' }
+      { key: 'index',   icon: 'd',  title: '個人簡介', subtitle: 0 },
+      { key: 'dev',     icon: 'e',  title: '開發心得', subtitle: 0 },
+      { key: 'mac',     icon: '1b', title: '蘋果環境', subtitle: 0 },
+      { key: 'aws',     icon: '22', title: 'AWS筆記', subtitle: 0 },
+      { key: 'note',    icon: '1e', title: '隨筆雜記', subtitle: 0 },
+      { key: 'mazu',    icon: '1a', title: '鄉土北港', subtitle: 0 },
+      { key: 'unbox',   icon: 'f',  title: '開箱文章', subtitle: 0 },
+      { key: 'album',   icon: '14', title: '生活相簿', subtitle: 0 },
+      { key: 'license', icon: '16', title: '授權聲明', subtitle: 0 }
     ],
   },
   mounted () {
     this.load(this.items
-      .map(t => (t.url = 'index' + EXT + '?k=' + t.k, t))
-      .filter(({ k }) => Params.k === k).shift() || this.items[0])
+      .map(t => (t.url = 'index' + EXT + '?k=' + t.key, t))
+      .filter(({ key }) => Params.k === key).shift() || this.items[0])
   },
   methods: {
     load (item) {
       API.Q
         .enqueue(next => next(this.item = item))
-        .enqueue(next => API.GET('/api/' + this.item.k + '.json').fail(_ => this.is404 = true).done(next))
+        .enqueue(next => API.GET('/api/' + this.item.key + '.json').fail(_ => this.is404 = true).done(next))
         .enqueue((next, main) => {
           if (!main.items) return next(main)
           
           const i = main.items
-            .map(t => (t.url = '/index' + EXT + '?k=' + this.item.k + '&id=' + t.k, t))
-            .map(({ k }) => k)
+            .map(item => (item.url = '/index' + EXT + '?k=' + this.item.key + '&id=' + item.id, item))
+            .map(({ id }) => id)
             .indexOf(decodeURI(Params.id))
 
           if (i == -1) return next(main, Params.id && Toastr.failure('找不到資料！'))
@@ -188,10 +186,12 @@ Load.init({
           const item = main.items[i]
           const p = i == 0 ? null : main.items[i - 1]
           const n = i == main.items.length - 1 ? null : main.items[i + 1]
-          return item ? API.GET('/api/' + this.item.k + '/' + item.k + '.json').done(main => next({ ...main, p, n })).fail(_ => next(main, Toastr.failure('找不到資料！'))) : next(main)
+          const other = p || n ? { prev: p, next: n } : {}
+
+          return item ? API.GET('/api/' + this.item.key + '/' + item.id + '.json').done(main => next({ ...main, other })).fail(_ => next(main, Toastr.failure('找不到資料！'))) : next(main)
         })
         .enqueue((next, main) => next(this.main = main))
-        .enqueue(next => next(this.items.forEach(item => API.GET('/api/' + item.k + '.json').done(({ s }) => item.st = s && s.length).send())))
+        .enqueue(next => next(this.items.forEach(item => API.GET('/api/' + item.key + '.json').done(({ items = 0 }) => item.subtitle = items && items.length).send())))
     }
   },
   template: El.render(`
@@ -199,8 +199,8 @@ Load.init({
       toastr
 
       div#menu => :class={ on: on }
-        a => *for=t in items   :key=t.k   :class=['icon-' + t.c, { active: item === t }]   :href=t.url
-          span => *text=t.t   :text=t.st
+        a => *for=t in items   :key=t.k   :class=['icon-' + t.icon, { active: item === t }]   :href=t.url
+          span => *text=t.title   :text=t.subtitle
 
       header#header
         label.icon-0 => @click=on=true
